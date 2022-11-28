f52c6fcf580d213b6b1f2d770df8560112cf997b
Andrew <aforest41@yahoo.com>
2022-11-28 08:41:49 -0800

    commit tree 47e1731d05422d3246139b47dd61327adda9a7f7

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Code your solution here
+function findMatching(drivers, match){
+    return drivers.filter((el) => el.toLowerCase() === match.toLowerCase());
+}
+
+function fuzzyMatch(drivers, match){
+    return drivers.filter((el) => el.toLowerCase().startsWith(match.toLowerCase()));
+}
+
+function matchName(drivers, match){
+    let driversMatching = drivers.filter((el) => el.name === match);
+    return driversMatching;
+}
+
+/* const users = [
+    {
+      firstName: "Niky",
+      lastName: "Morgan",
+      favoriteColor: "Blue",
+      favoriteAnimal: "Jaguar",
+    },
+    {
+      firstName: "Tracy",
+      lastName: "Lum",
+      favoriteColor: "Yellow",
+      favoriteAnimal: "Penguin",
+    },
+    {
+      firstName: "Josh",
+      lastName: "Rowley",
+      favoriteColor: "Blue",
+      favoriteAnimal: "Penguin",
+    },
+    {
+      firstName: "Kate",
+      lastName: "Travers",
+      favoriteColor: "Red",
+      favoriteAnimal: "Jaguar",
+    },
+    {
+      firstName: "Avidor",
+      lastName: "Turkewitz",
+      favoriteColor: "Blue",
+      favoriteAnimal: "Penguin",
+    },
+    {
+      firstName: "Drew",
+      lastName: "Price",
+      favoriteColor: "Yellow",
+      favoriteAnimal: "Elephant",
+    },
+  ];
+
+  console.log(users.filter((el) => el.firstName === 'Niky')); */
